68ccbd9cf8f055c939f13b0ea500246e0a431de1
ArunKumarsece <136149813+ArunKumarsece@users.noreply.github.com>
2023-06-22 18:10:31 +0530

    Update search in 2D matrix 1 and 2.py

--- search in 2D matrix 1 and 2.py
+++ search in 2D matrix 1 and 2.py
@@ -5,3 +5,24 @@ class Solution:
                 return True
         return False
 
+// Another Approach Using Binary Search
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        list1 = []
+        for row in matrix:
+            if row[-1] >= target:
+                list1 = row
+                break
+        l,r = 0,len(list1)-1
+        while l <= r:
+            mid = (l+r) // 2
+            if list1[mid] == target:
+                return True
+            elif list1[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return False
+
+        
